3a23e7cf8df2d6999dbbc5b776a339f93d7bdae7
Paul Haeussler <paul.haeussler@rib-ims.com>
2022-05-09 13:55:52 +0200

    .

--- app.js
+++ app.js
@@ -38,4 +38,58 @@ window.onload = function I() {
   var showLives = document.getElementById("mylives");
   var getHint = document.getElementById("hint");
   var showClue = document.getElementById("clue");
+
+  // create alphabet ul 
+  var buttons = function () {
+      myButtons = document.getElementById('buttons');
+      letters = document.createElement('ul');
+
+      for (var i = 0; i < alphabet.length; i++) {
+          letters.id = 'alphabet';
+          list = document.createElement('li');
+          list.id = 'letter';
+          list.innerHTML = alphabet[i];
+          check();
+          myButtons.appendChild(letters);
+          letters.appendChild(list);
+      }
+  }
+
+  // create guesses ul
+
+  result = function () {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+
+  // show lives 
+  comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+  
 };
